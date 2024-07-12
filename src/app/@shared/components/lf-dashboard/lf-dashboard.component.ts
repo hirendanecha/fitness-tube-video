4fd5013ab270495453068e945c01b7f4ffcf4834
ppa-opash <ppa.opash@gmail.com>
2024-07-12 10:38:37 +0530

    Apply Changes

--- src/app/@shared/components/lf-dashboard/lf-dashboard.component.ts
+++ src/app/@shared/components/lf-dashboard/lf-dashboard.component.ts
@@ -163,6 +163,8 @@ export class LfDashboardComponent implements OnInit {
     this.commonService.get(apiUrl).subscribe({
       next: (res) => {
         this.channelList = res.data;
+        let channelIds = this.channelList.map(e => e.id);
+        localStorage.setItem('get-channels', JSON.stringify(channelIds));
         console.log(this.channelList);
       },
       error(err) {
